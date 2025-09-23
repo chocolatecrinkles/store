e64009e8e7e26216513a7b0dfbc9af9ffaebdbe3
krizza <krizzabeth.olodin@cit.edu>
2025-09-23 09:46:34 +0800

    feature(store): setup static files

--- store/store/settings.py
+++ store/store/settings.py
@@ -15,6 +15,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # e.g. project-level "static" folder
+]
+
+# For production: where collectstatic will gather all files
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
